cd7af4ef2828d08eda9e96f0f751422c1da26d72
Vova Katz <vladimir@useintegral.com>
2025-02-28 17:06:22 -0500

    modify template laoding

--- app/pages/home.py
+++ app/pages/home.py
@@ -7,4 +7,4 @@ router = APIRouter()
 
 @router.get("/", response_class=HTMLResponse)
 async def home(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    return templates.TemplateResponse(request, "index.html")
